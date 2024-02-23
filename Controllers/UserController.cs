091acd645d1ef6be04d240a55afbaf6e052942cc
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-02-23 14:03:44 -0300

    estrutura base

---
+++ Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie;
+
+public class UserController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Welcome()
+    {
+        return View();
+    }
+
+
+}
---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace MvcMovie.Models;
+
+public class User
+{
+    public required string Username { get; set; }
+    public required string Password { get; set; }
+    public required string Email { get; set; }
+
+}
